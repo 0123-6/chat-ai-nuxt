8a697cbba6698e2fcb4526dda817124ba910ec6b
90945 <909458209@qq.com>
2025-12-25 20:42:07 +0800

    开发基础布局组件

---
+++ app/layouts/default.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="w-screen h-screen flex flex-col overflow-auto">
+    <div class="w-full min-w-200 h-full">
+      <slot/>
+    </div>
+  </div>
+</template>
