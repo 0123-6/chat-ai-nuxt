378492027655392d47de1d832e8e8935dd83f765
90945 <909458209@qq.com>
2025-12-25 20:43:51 +0800

    开发基础布局组件

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div class="w-screen h-screen flex flex-col overflow-auto">
-    <div class="w-full min-w-200 h-full">
+    <div class="w-full min-w-200 h-full flex flex-col">
       <slot/>
     </div>
   </div>
