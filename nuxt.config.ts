02c3c44067253d9e915a7dde58b5fce118a5510f
90945 <909458209@qq.com>
2025-12-26 20:42:56 +0800

    添加br压缩静态资源

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import tailwindcss from "@tailwindcss/vite";
+import compression, {defineAlgorithm} from "vite-plugin-compression2";
+import zlib from 'node:zlib'
 
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
@@ -7,6 +9,21 @@ export default defineNuxtConfig({
   vite: {
     plugins: [
       tailwindcss(),
+      // 压缩插件
+      compression({
+        algorithms: [
+          defineAlgorithm(
+            'brotliCompress',
+            {
+              params: {
+                [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+              },
+            },
+          ),
+        ],
+        // 压缩后的文件名称
+        filename: '[path][base].br',
+      }),
     ],
   },
   css: [
